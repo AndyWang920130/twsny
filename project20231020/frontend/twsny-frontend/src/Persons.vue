cb9d16f42144ea595620d1d86a6eeb73d5aa6ca0
wanghf <wanghf@jousing.com>
2023-11-08 22:25:58 +0800

    update for vue

---
+++ project20231020/frontend/twsny-frontend/src/Persons.vue
@@ -0,0 +1,37 @@
+<!--
+该示例创建了一个可复用网格组件，并结合外部数据使用它。
+-->
+
+<script setup>
+    import DemoGrid from './components/Grid.vue'
+    import { ref, watchEffect } from 'vue'
+
+    const searchQuery = ref('')
+    const gridColumns = ['name', 'power']
+    const gridData = [
+        { name: 'Chuck Norris', power: Infinity },
+        { name: 'Bruce Lee', power: 9000 },
+        { name: 'Jackie Chan', power: 7000 },
+        { name: 'Jet Li', power: 8000 }
+    ]
+
+    const API_URL = "http://211.149.248.84:8080/api/v1/persons"
+    const result = ref(null)
+    watchEffect(async () => {
+        // 该 effect 会立即运行，
+        // 并且在 currentBranch.value 改变时重新运行
+        const url = `${API_URL}`
+        result.value = await (await fetch(url)).json()
+    })
+</script>
+
+<template>
+    <form id="search">
+        Search <input name="query" v-model="searchQuery">
+    </form>
+    <DemoGrid
+            :data="gridData"
+            :columns="gridColumns"
+            :filter-key="searchQuery">
+    </DemoGrid>
+</template>
--- project20231020/frontend/twsny-frontend/src/main.js
+++ project20231020/frontend/twsny-frontend/src/main.js
@@ -1,6 +1,16 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
-import App from './App.vue'
 
-createApp(App).mount('#app')
+import App from './App.vue'
+import App1 from './App1.vue'
+import App2 from './App2.vue'
+import Persons from './Persons.vue'
+import FetchData from './FetchData.vue'
+
+
+// createApp(App).mount('#app')
+// createApp(App1).mount('#app')
+// createApp(App2).mount('#app')
+// createApp(Persons).mount('#app')
+createApp(FetchData).mount('#app')
